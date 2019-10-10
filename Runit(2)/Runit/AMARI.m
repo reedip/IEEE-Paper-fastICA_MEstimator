49a276b3a420b85e593108984dba0de66c291975
BANERJEE REEDIP <JP26022@w022581902978m.local>
2019-10-10 17:34:41 +0900

    Update Directory Structure

--- Runit(2)/Runit/AMARI.m
+++
@@ -1,4 +0,0 @@
-function a = AMARI( W,CC,P)% function a = AMARItest( G)%% Returns a performance measure based on the system’s global matrix using the AMARI criterion% Cichocki, A and Amari, S "Adaptive Blind Signal and Image Processing"% John Wiley & Sons 2005 p308
-G=W*CC*P;
-a = sum(sum(abs(G),2)./max(abs(G),[],2)-1) + sum(sum(abs(G))./max(abs(G))-1);
-a = a/size(G,1);
--- Runit(2)/Runit/signal.m
+++
@@ -1,8 +0,0 @@
-function [ sqrtN ]= signal(N)
-sqrtN=ones(1,N);
-for i=1:N
-sqrtN(i)=sin(i);
-end
-%i=1:1000;
-%plot(i,sqrtN)
-%axis ( [ 0 100 -1 1 ])
